241ed56fb477dedecff6a0f47185515a8ddcaa9e
Sunitha <sunita.qaengg@gmail.com>
2024-03-05 15:55:37 +1300

    version2

--- src/Gitpackage/Test1.java
+++ src/Gitpackage/Test1.java
@@ -4,6 +4,14 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String hungry="yes";
+		if (hungry=="yes")
+			System.out.println("eat something");
+			else
+			{
+				System.out.println("Do work");
+			}
+		
 
 	}
 
